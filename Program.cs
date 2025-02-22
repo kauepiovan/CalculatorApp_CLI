e39fea303e0030e9c3350a6c7e5464d036ac3a2a
Kaue <kaupiovan@gmail.com>
2025-02-21 23:00:40 -0300

    init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.WriteLine("Digite um valor: ");
+        float v1 = float.Parse(Console.ReadLine());
+        
+        Console.WriteLine("\nDigite outro valor: ");
+        float v2 = float.Parse(Console.ReadLine());
+
+        float resultado = v1 + v2;
+        
+        Console.WriteLine($"\nO resultado de {v1} + {v2} = {resultado}");
+
+        
+    }
+}
